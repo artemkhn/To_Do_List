256ab730b6415bd40ccc084e87e3ca05daa84f2d
Artem Khan <akhan195@my.bcit.ca>
2022-08-15 21:06:54 -0700

    Added feature that allows users to cross the tasks that are done and did some styling

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import '../css/Input.css'
+import Delete from '../images/delete.png';
+
 
 export default function Input() {
     const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
@@ -11,27 +13,37 @@ export default function Input() {
             text: input,
             done: false
         }
-        setTasks([...tasks, createTask]);
-        setInput('');
+        if(input !== "") {
+            setTasks([...tasks, createTask]);
+            setInput('');
+        }
     }
     const removeTask = (id) => {
         setTasks(tasks.filter(task => task.id !== id));
+    }
+    const taskComplete = (id) => {
+        setTasks(
+            tasks.map(task => (
+                task.id === id ? {...task, done: !task.done} : task
+            ))
+        )
     }
 
     useEffect(() => {
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }, [tasks])
   return (
-    <div>
+    <div className='mainContainer'>
         <form onSubmit={addInput}>
             <input onChange={event => setInput(event.target.value)} value={input} type="text" placeholder="Task"/>
             <button>Add</button>
         </form>
-        <div>
+        <div id="taskC">
             {tasks.map(task => (
-                <div key={task.id}>
-                    <p>{task.text}</p>
-                    <button onClick={() => removeTask(task.id)}>Remove</button>
+                <div key={task.id} className="task">
+                    <p className={task.done ? 'doneTask' : 'eachTask'}>{task.text}</p>
+                    <img onClick={() => removeTask(task.id)} src={Delete} alt="garbage" className="eachImage"/>
+                    <input className="eachCheck" onChange={() => taskComplete(task.id)} type='checkbox' defaultChecked={task.done ? true : false} />
                 </div>
             ))}
         </div>
